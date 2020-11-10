50a335e27c0b0273e9ec1784ffbb529fc7d90c59
Ashley perkins <perkinsashley26@gmail.com>
2020-11-09 23:40:43 -0500

    Completion of Exercise 2

---
+++ app.js
@@ -0,0 +1,17 @@
+window.onload = function(){
+    let search= document.getElementById("btn");
+    search.addEventListener("click", function(x){
+        x.preventDefault();
+        Fetch();
+    });
+
+
+const url = "http://localhost/info2180-lab4/superheroes.php";
+
+async function Fetch(){
+    let response = await fetch(url);
+    let data = await response.text();
+    alert( data);
+ 
+}
+}
